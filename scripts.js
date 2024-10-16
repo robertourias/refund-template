528f7325ff054a8c8ef86c0ff2b7bf6acf0691a6
Roberto Nicoletti <roberto.urias@gmail.com>
2024-10-15 22:26:18 -0300

    removendo item da lista

--- scripts.js
+++ scripts.js
@@ -153,3 +153,21 @@ function updateTotals() {
         console.log(error)
     }
 }
+
+// Evento que captura o clique nos itens da lista
+expenseList.addEventListener("click", function (event) {
+
+    // Verificar se o elemento clicado é o ícone de remover
+    if (event.target.classList.contains("remove-icon")) {
+        
+        // Obtém a li pai do elemento clicado
+        const item = event.target.closest(".expense")
+
+        // Remove o item da lista
+        item.remove()
+    }
+
+    // Atualiza os totais
+    updateTotals()
+
+})
